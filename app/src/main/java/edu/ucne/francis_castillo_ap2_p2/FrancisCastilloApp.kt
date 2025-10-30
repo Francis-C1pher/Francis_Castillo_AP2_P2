407a7984b26550a9675f61d1f23ef149c24ec8c8
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-10-29 20:13:11 -0400

    Configurando Hilt y dando permiso para que tenga acceso a internet.

---
+++ app/src/main/java/edu/ucne/francis_castillo_ap2_p2/FrancisCastilloApp.kt
@@ -0,0 +1,8 @@
+package edu.ucne.francis_castillo_ap2_p2
+
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FrancisCastilloApp : Application()
--- app/src/main/java/edu/ucne/francis_castillo_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/francis_castillo_ap2_p2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.francis_castillo_ap2_p2.ui.theme.Francis_Castillo_AP2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
